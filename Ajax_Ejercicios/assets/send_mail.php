4817123359d835c15d349a22acc38b3259c84233
Keiberth <keiberth456@gmail.com>
2020-12-22 12:42:31 -0400

    Ejercicio 6 Ajax Contact_form

--- Ajax_Ejercicios/assets/send_mail.php
+++ Ajax_Ejercicios/assets/send_mail.php
@@ -8,7 +8,7 @@ if (isset($_POST)) {
   $comments = $_POST["comments"];
 
   $domain = $_SERVER["HTTP_HOST"];
-  $to = "jonmircha@gmail.com";
+  $to = "keiberth456@gmail.com";
   $subject_mail = "Contacto desde el formulario del sitio $domain.";
   $message = "
     <p>

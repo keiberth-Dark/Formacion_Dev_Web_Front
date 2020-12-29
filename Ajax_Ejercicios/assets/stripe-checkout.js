7bae838820302447dc7d79e826a72207ce250007
Keiberth <keiberth456@gmail.com>
2020-12-29 13:28:47 -0400

    Ejercicio de Stripe 1-1

--- Ajax_Ejercicios/assets/stripe-checkout.js
+++ Ajax_Ejercicios/assets/stripe-checkout.js
@@ -1,4 +1,44 @@
+
+
+
 /* ********** Curso JavaScript: 129. Ejercicios AJAX - APIs: Pagos Online con Fetch y Stripe (1/4) - #jonmircha ********** */
+import STRIPE_KEYS from "./stripe-keys.js";
+
+//console.log(STRIPE_KEYS);
+
+const d = document,
+    $tacos = d.getElementById("tacos"),
+    $template = d.getElementById("taco-template").content,
+    $fragment = d.createDocumentFragment();
+
+fetch("https://api.stripe.com/v1/products",{
+  headers: {
+    Authorization: `Bearer ${STRIPE_KEYS.secret}`,
+  },
+}).then((res) => {
+  //console.log(res);
+  return res.json()
+})
+.then(json => {
+  //console.log(json);
+});
+
+
+fetch("https://api.stripe.com/v1/prices",{
+  headers: {
+    Authorization: `Bearer ${STRIPE_KEYS.secret}`,
+  },
+}).then((res) => {
+  console.log(res);
+  return res.json()
+})
+.then(json => {
+  console.log(json);
+});
+/* Invocar 2 llamadas fetch no es obtimo por eso usamos promesas old 
+que nos permite llamar a 2 y cuando esten listas mostrar una sola */
+
+
 /* ********** Curso JavaScript: 130. Ejercicios AJAX - APIs: Pagos Online con Fetch y Stripe (2/4) - #jonmircha ********** */
 /* ********** Curso JavaScript: 131. Ejercicios AJAX - APIs: Pagos Online con Fetch y Stripe (3/4) - #jonmircha ********** */
 /* ********** Curso JavaScript: 132. Ejercicios AJAX - APIs: Pagos Online con Fetch y Stripe (4/4) - #jonmircha ********** */
--- Ajax_Ejercicios/assets/stripe-keys.js
+++ Ajax_Ejercicios/assets/stripe-keys.js
@@ -1,4 +1,7 @@
+
+
+
 export default {
-  public: "Your Public Key",
-  secret: "Your Secret Key",
+  public: "pk_test_51I2LPzEymUGYdrWBsLGhRWom32ZklBX8CXVrE1DLMfw4zV6KP2JXOX8KdBkFYtTE1wPkoScicWIoWJfmE1FD4t9a00lSy9EH7s",
+  secret: "sk_test_51I2LPzEymUGYdrWBC6PISmsd33FsVCZDgY1YkJJAhVetAIJOtR0YgHjA3h0O37nGvTiBJkpdagiPbDKeRcExft6L00ndFG1Fzp",
 };

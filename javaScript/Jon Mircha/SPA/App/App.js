b984a5c9e9f5830e8eece094f5f4003a4f6967c9
Keiberth <keiberth456@gmail.com>
2021-01-13 18:48:17 -0400

    Ejercicio SPA (4/)

--- javaScript/Jon Mircha/SPA/App/App.js
+++ javaScript/Jon Mircha/SPA/App/App.js
@@ -1,8 +1,10 @@
-import api from "./Helpers/wp_api.js"
-import {ajax} from "./Helpers/ajax.js"
+import api from "./Helpers/wp_api.js";
+import { ajax } from "./Helpers/ajax.js";
+import { Title } from "./Components/Title.js";
+import { Loader } from "./Components/Loader.js";
 
-export function App(){
-  document.getElementById("root").innerHTML = `<h1>Bienvenidos a SPA con Vanilla-Js</h1>`
+export function App() {
+  /* document.getElementById("root").innerHTML = `<h1>Bienvenidos a SPA con Vanilla-Js</h1>`
 
   ajax({
     url: api.POSTS,
@@ -18,6 +20,10 @@ export function App(){
     cbSuccess: (categories)=>{
       console.log("Categorias",categories);
     }
-  });
-  
+  }); */
+  const d = document,
+    $root = d.getElementById("root");
+
+  $root.appendChild(Title());
+  $root.appendChild(Loader());
 }
